bceae4661e7f6f6e1e76668e6777950f61d8998e
freestraws <stephdonnelly1@gmail.com>
2018-05-06 16:31:44 -0400

    collectibles are worth points now, that's cool

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			GameManager.instance.addPoint();
+			gameObject.SetActive(false);
+		}
+	}
+}
