3e7e9b523d85f6a3c3402704486463315fc4668c
freestraws <stephdonnelly1@gmail.com>
2018-05-06 16:29:58 -0400

    tile collision does things now for things tagged as player

---
+++ Assets/Scripts/CrackedTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackedTile : NormalTile {
+
+	public List<Material> crackedLookProgression;
+	int crackProgression;
+
+	// Use this for initialization
+	void Start () {
+		crackProgression = 0;
+		base.CollisionEnterEvent += crack;
+		//base.CollisionStayEvent += crack;
+	}
+
+	void crack(){
+		crackProgression++;
+		GetComponent<Renderer>().material = crackedLookProgression[crackProgression];
+	}
+}
---
+++ Assets/Scripts/NormalTile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalTile : MonoBehaviour {
+
+	float timeColliding = 0;
+
+	public delegate void EventHandler();
+	public event EventHandler CollisionEnterEvent = () => {};
+	public event EventHandler CollisionStayEvent = () => {};
+	public event EventHandler CollisionExitEvent = () => {};
+
+	void OnCollisionEnter(Collision collision){
+		foreach(ContactPoint contact in collision){
+			if(contact.otherCollider.gameObject.CompareTag("Player")){
+				Debug.Log("player touch tile, what do?");
+				CollisionEnterEvent();
+			}
+		}
+	}
+
+	void OnCollisionStay(Collision collision){
+		foreach(ContactPoint contact in collision){
+			if(contact.otherCollider.gameObject.CompareTag("Player")){
+				Debug.Log("player continuing to touch tile, what do?");
+				timeColliding+=Time.deltaTime;
+			}
+		}
+	}
+
+	void OnCollisionExit(Collision collision){
+		foreach(ContactPoint contact in collision){
+			if(contact.otherCollider.gameObject.CompareTag("Player")){
+				Debug.Log("player stopped touching tile, what do?");
+				timeColliding = 0;
+			}
+		}
+	}
+
+	void Delete(){
+		Destroy(this.gameObject);
+	}
+}
